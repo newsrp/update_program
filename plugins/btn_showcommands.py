c331697eebcae1770ccb8c98516b091fa4b08e76
Nikita <nikitabikmaev@gmail.com>
2018-08-20 01:19:40 +0300

    Update btn_showcommands.py

--- plugins/btn_showcommands.py
+++ plugins/btn_showcommands.py
@@ -3,7 +3,7 @@ from plugin import Plugin
 class ShowCommands(Plugin):
 
     def OnCommand(self, objects):
-        self.bot.api.messages.send(user_id=objects['from_id'], message='Временно не работает')
+        self.bot.api.messages.send(user_id=objects['from_id'], message='Временно не работает..')
 
     buttons = ['show_commands']
  
