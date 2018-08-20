aba3697b1bb0aeadf4ecc548b565799b33b74c3b
Nikita <nikitabikmaev@gmail.com>
2018-08-21 00:09:16 +0300

    Update test.py

--- plugins/test.py
+++ plugins/test.py
@@ -5,7 +5,7 @@ class PluginTest(Plugin):
     def OnCommand(self, objects):
         self.bot.api.messages.send(user_id=objects['from_id'], message='Yes')
 
-    commands = ['wffrfee1']
+    commands = ['frkwfjegjwrjgwgjtrgjtrjrtwgrtgrw']
     
     enabled = False
  
